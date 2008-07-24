1337c6f210bbc286b4acd3759449376f67de623d
arlo.phoenix <arlo.phoenix@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-07-24 09:33:42 +0000

    trying to upload an integrated copy of the project such that someone can download and test it out.

---
+++ integration/userauth/WebContent/confirmation.jsp
@@ -0,0 +1,21 @@
+<%@page contentType="text/html; charset=ISO-8859-1" %>
+<html>
+	<head>
+		<title> Ride Successfully Added! </title>
+		<style type="text/css" media="screen">@import "3ColumnLayout.css";</style>
+	</head>
+	<body>
+
+	<%@ include file="heading.html" %>
+
+		<div class="content">
+			<p>Thank you for adding a ride from <%= request.getParameter("from1") %>, to <%= request.getParameter("to1") %>;  
+			scheduled for <%= request.getParameter("time1") %> <%= request.getParameter("date1") %>. </p>
+		</div>
+
+	<%@ include file="leftMenu.html" %>
+
+	<%@ include file="rightMenu.jsp" %>
+
+	</body>
+</html>
---
+++ integration/userauth/src/car_pool/user/database/CarPoolUserManager.java
@@ -0,0 +1,79 @@
+package car_pool.user.database;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.verisign.joid.Store;
+import org.verisign.joid.db.DbStore;
+import org.verisign.joid.server.MemoryUserManager;
+import org.verisign.joid.server.OpenIdServlet;
+import org.verisign.joid.server.User;
+import org.verisign.joid.server.UserManager;
+
+public class CarPoolUserManager implements UserManager {
+
+	Map<String, User> users = new Hashtable<String, User>();
+	Store store = new DbStore();
+	
+	private MemoryUserManager userManager()
+    {
+		
+        return (MemoryUserManager) OpenIdServlet.getUserManager();
+    }
+	
+	public CarPoolUserManager() {
+		addUser("James", "password");
+		addUser("Arlo", "password");
+		addUser("Ben", "password");
+		addUser("Jordan", "password");
+		addUser("Parul", "password");
+		addUser("Sam", "password");
+	}
+	
+	protected void addUser(String name, String password) {
+		User record = new User();
+		record.setUsername(name);
+		record.setPassword(password);
+		userManager().save(record);
+	}
+	
+	public void saveUser(User user) {
+		userManager().save(user);
+	}
+	
+	public User getUser( String name ) throws NoSuchUserException {
+		User user = null;
+		try {
+			//user = users.get(name);
+			user = userManager().getUser(name);
+		} catch(NullPointerException e) {
+			throw new NoSuchUserException();
+		}
+//		if( user == null ) {
+//			throw new NoSuchUserException();
+//		}
+		return user;
+	}
+
+	@Override
+	public boolean canClaim(String username, String claimedId) {
+		String usernameFromClaimedId = claimedId.substring(claimedId.lastIndexOf("/") + 1);
+        if (username.equals(usernameFromClaimedId)) {
+            return true;
+        }
+		return false;
+	}
+
+	@Override
+	public String getRememberedUser(String username, String authKey) {
+		if (username == null || authKey == null) return null;
+		return userManager().getRememberedUser(username, authKey);
+		//return null;
+	}
+
+	@Override
+	public void remember(String username, String authKey) {
+		// TODO Auto-generated method stub
+		userManager().remember(username, authKey);
+	}
+}
