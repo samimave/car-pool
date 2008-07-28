d95b69f92a7cce151cd0154a59a87eed6900ac7d
jordan.d.carter <jordan.d.carter@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-07-28 20:29:20 +0000

    Persistance - added removeRide(user, ride)/(ride) to take a person out of a carpool and to remove the trip all together.

---
+++ trunk/persistenceModel/src/sql/createDB_0_12.sql
@@ -0,0 +1,93 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+CREATE SCHEMA IF NOT EXISTS `carpool` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `carpool`;
+
+-- -----------------------------------------------------
+-- Table `carpool`.`User`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `carpool`.`User` ;
+
+CREATE  TABLE IF NOT EXISTS `carpool`.`User` (
+  `idUser` INT NOT NULL AUTO_INCREMENT ,
+  `userName` VARCHAR(45) NOT NULL ,
+  `userPasswordHash` VARCHAR(150) NOT NULL ,
+  `signUpDate` DATE NOT NULL ,
+  `ridesGiven` INT NOT NULL DEFAULT 0 ,
+  `ridesTaken` INT NOT NULL DEFAULT 0 ,
+  PRIMARY KEY (`idUser`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `carpool`.`Ride`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `carpool`.`Ride` ;
+
+CREATE  TABLE IF NOT EXISTS `carpool`.`Ride` (
+  `idRide` INT NOT NULL AUTO_INCREMENT ,
+  `idUser` INT NOT NULL ,
+  `availableSeats` INT NOT NULL ,
+  `rideStartLocation` MEDIUMTEXT NOT NULL ,
+  `rideStopLocation` MEDIUMTEXT NOT NULL ,
+  `rideDate` DATE NOT NULL ,
+  PRIMARY KEY (`idRide`) ,
+  INDEX fk_Ride_User (`idUser` ASC) ,
+  CONSTRAINT `fk_Ride_User`
+    FOREIGN KEY (`idUser` )
+    REFERENCES `carpool`.`User` (`idUser` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `carpool`.`RideReport`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `carpool`.`RideReport` ;
+
+CREATE  TABLE IF NOT EXISTS `carpool`.`RideReport` (
+  `idRideReport` INT NOT NULL AUTO_INCREMENT ,
+  `idRide` INT NOT NULL ,
+  PRIMARY KEY (`idRideReport`) ,
+  INDEX fk_RideReport_Ride (`idRide` ASC) ,
+  CONSTRAINT `fk_RideReport_Ride`
+    FOREIGN KEY (`idRide` )
+    REFERENCES `carpool`.`Ride` (`idRide` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `carpool`.`Matches`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `carpool`.`Matches` ;
+
+CREATE  TABLE IF NOT EXISTS `carpool`.`Matches` (
+  `idTrip` INT NOT NULL AUTO_INCREMENT ,
+  `idRide` INT NOT NULL ,
+  `idUser` INT NOT NULL ,
+  `confirmed` BOOLEAN NOT NULL DEFAULT false ,
+  PRIMARY KEY (`idTrip`) ,
+  INDEX idRide (`idRide` ASC) ,
+  INDEX idUser (`idUser` ASC) ,
+  CONSTRAINT `idRide`
+    FOREIGN KEY (`idRide` )
+    REFERENCES `carpool`.`Ride` (`idRide` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `idUser`
+    FOREIGN KEY (`idUser` )
+    REFERENCES `carpool`.`User` (`idUser` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
