9c58f8083d94ee74895c1fc65e1d9a10389fdc3f
Jordan.D.Carter <Jordan.D.Carter@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-08-10 03:24:59 +0000

    Implemented rideListing

--- persistenceModel/src/car/pool/persistance/RideListing.java
+++ persistenceModel/src/car/pool/persistance/RideListing.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 public interface RideListing {
 	
 	public boolean next()throws SQLException;
-	public int getRideID();
-	public int getUserID();
-	public String getUsername();
-	public int getAvailableSeats();
-	public Date getRideDate();
-	public String getStartLocation();
-	public String getEndLocation();
+	public int getRideID() throws SQLException;
+	public int getUserID() throws SQLException;
+	public String getUsername() throws SQLException;
+	public int getAvailableSeats() throws SQLException;
+	public Date getRideDate() throws SQLException;
+	public String getStartLocation() throws SQLException;
+	public String getEndLocation() throws SQLException;
 }
