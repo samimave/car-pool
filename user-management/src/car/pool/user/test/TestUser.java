905a16da3b8686e0191f994b7784c399f8098023
terrasea <terrasea@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-08-12 00:49:24 +0000

    Started on test suite for User

---
+++ user-management/src/car/pool/user/test/TestUser.java
@@ -0,0 +1,160 @@
+package car.pool.user.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import car.pool.user.User;
+import car.pool.user.UserFactory;
+import car.pool.user.UserManager;
+
+public class TestUser {
+
+	static User user = null;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		user = UserFactory.getInstance("http://terrasea.pip.verisignlabs.com/");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		user = null;
+	}
+
+	@Test
+	public void testCalcSocialScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditDetail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmail() {
+		String email = user.getEmail();
+		Assert.assertEquals(email, "terrasea@gmail.com");
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOcupation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOpenIds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPhoneNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSocialScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSuburb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoginFailed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMemberSince() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddOpenId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMemberSince() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOcupation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPhoneNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSocialScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSuburb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserName() {
+		fail("Not yet implemented");
+	}
+
+}
