febe65aedea5a7dda48b86a96493986a5bb1d997
terrasea <terrasea@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-08-12 14:20:26 +0000

    Some modifications to the User stuff allowing for deletion of Users as well as getting user info such as username, etc and a test case to test it.

--- user-management/src/car/pool/user/UserManager.java
+++ user-management/src/car/pool/user/UserManager.java
@@ -5,6 +5,7 @@ import car.pool.persistance.CarPoolStore;
 import car.pool.persistance.CarPoolStoreImpl;
 import car.pool.persistance.exception.DuplicateUserNameException;
 import car.pool.persistance.exception.InvaildUserNamePassword;
+import car.pool.persistance.exception.StoreException;
 import car.pool.persistance.exception.UserException;
 
 import java.io.IOException;
@@ -12,19 +13,26 @@ import java.io.IOException;
 public class UserManager {
 
 	public User login(String openid) throws InvaildUserNamePassword, IOException {
-		return UserFactory.getInstance(openid);
+		return UserFactory.newInstance(openid);
 	}
 	
+
 	public User registerUser(User user) throws DuplicateUserNameException, UserException, IOException {
 		CarPoolStoreImpl store = new CarPoolStoreImpl();
-		
-		int id = store.addUser(user.getUserName(), user.getEmail(), user.getPhoneNumber(),"");
+		if(store.checkUserExists(user.getUserName())) {
+			throw new DuplicateUserNameException("I have checked and you already exist");
+		}
+		int id = store.addUser((String)user.getOpenIds().toArray()[0], user.getUserName(), user.getEmail(), user.getPhoneNumber());
 		for(String openid : user.getOpenIds()) {
-			store.attachOpenID(openid, id);
+			if(!((String)user.getOpenIds().toArray()[0]).equals(openid)) {
+				store.attachOpenID(openid, id);
+			}
 		}
 		
 		user.setUserId(id);
-		
+
+		System.out.println("User ID: " + user.getUserId());		
+
 		return user;
 	}
 	
@@ -34,5 +42,15 @@ public class UserManager {
 		user.addOpenId(openid);
 		
 		return user;
+	}
+	
+	public boolean removeUser( User user ) throws IOException, StoreException {
+		CarPoolStore store = new CarPoolStoreImpl();
+		for(String openid : user.getOpenIds()) {
+			System.out.println(openid);
+			store.detachOpenID(openid,user.getUserId());
+		}
+		
+		return store.removeUser(user.getUserName(), user.getPassword());
 	}
 }
---
+++ user-management/src/car/pool/user/test/UserManagerTest.java
@@ -0,0 +1,74 @@
+package car.pool.user.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import car.pool.user.UserManager;
+import car.pool.user.User;
+import car.pool.user.UserFactory;
+
+public class UserManagerTest {
+
+	UserManager manager = null;
+
+	@Before
+	public void setUp() throws Exception {
+		manager = new UserManager();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		manager = null;
+	}
+
+	@Test
+	public void testRegisterUser() {
+		User user = UserFactory.newInstance();
+		user.setEmail("terrasea@gmail.com");
+		user.setName("James");
+		user.setPhoneNumber("3530079");
+		user.setUserName("james");
+		user.addOpenId("http://terrasea.pip.verisignlabs.com");
+
+		try {
+			user = manager.registerUser( user );
+			Assert.assertNotNull(user.getUserId());
+		} catch(Exception e ) {
+			System.out.println(e);
+			fail("testRegisterUser: " + e);
+		}
+	}
+
+	/*@Test
+	public void testLogin() {
+		try {
+			User user = manager.login("http://terrasea.pip.verisignlabs.com");
+			System.out.println(user.getUserId());
+			Assert.assertNotNull(user.getUserId());
+		} catch(Exception e ) {
+			fail("testLogin" + e);
+		}
+	}
+
+	@Test
+	public void testAttachOpenId() {
+		fail("Not yet implemented");
+	}*/
+	
+	@Test
+	public void testRemoveUser() {
+		try {
+			User user = manager.login("http://terrasea.pip.verisignlabs.com");
+			if(user.getUserId() != null ) {
+				Assert.assertTrue(manager.removeUser(user));
+			}
+		} catch( Exception e ) {
+			fail("testRemoveUser: " + e);
+		}
+	}
+
+}
