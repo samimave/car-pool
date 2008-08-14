194d677852e6a7e2e054f58accc8b36b7ca29cc9
terrasea <terrasea@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-08-14 09:32:06 +0000

    Replaced these files with the ones that work.  The ones in the repository now are the old ones.

--- trunk/dev/web/src/car/pool/user/UserManager.java
+++ trunk/dev/web/src/car/pool/user/UserManager.java
@@ -1,6 +1,7 @@
 package car.pool.user;
 
-
+import car.pool.persistance.Database;
+import car.pool.persistance.DatabaseImpl;
 import car.pool.persistance.CarPoolStore;
 import car.pool.persistance.CarPoolStoreImpl;
 import car.pool.persistance.exception.DuplicateUserNameException;
@@ -9,24 +10,22 @@ import car.pool.persistance.exception.StoreException;
 import car.pool.persistance.exception.UserException;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UserManager {
 
-	public User login(String openid) throws InvaildUserNamePassword, IOException {
+	public User getUserByOpenId(String openid) throws InvaildUserNamePassword, IOException {
 		return UserFactory.newInstance(openid);
 	}
 	
 
-	public User registerUser(User user) throws DuplicateUserNameException, UserException, IOException {
+	public User registerUser(User user) throws DuplicateUserNameException, UserException, IOException, SQLException {
 		CarPoolStoreImpl store = new CarPoolStoreImpl();
-		if(store.checkUserExists(user.getUserName())) {
-			throw new DuplicateUserNameException("I have checked and you already exist");
-		}
-		int id = store.addUser((String)user.getOpenIds().toArray()[0], user.getUserName(), user.getEmail(), user.getPhoneNumber());
+		Integer id = new Integer(store.addUserWithPassword(user.getUserName(), user.getEmail(), user.getPhoneNumber(), "n/a"));
 		for(String openid : user.getOpenIds()) {
-			if(!((String)user.getOpenIds().toArray()[0]).equals(openid)) {
-				store.attachOpenID(openid, id);
-			}
+			store.attachOpenID(openid, id);
+			break;
 		}
 		
 		user.setUserId(id);
@@ -36,21 +35,39 @@ public class UserManager {
 		return user;
 	}
 	
-	public User attachOpenId(String openid, User user) throws IOException {
+	public User attachOpenId(String openid, User user) throws IOException, SQLException {
 		CarPoolStore store = new CarPoolStoreImpl();
 		store.attachOpenID(openid, user.getUserId());
 		user.addOpenId(openid);
 		
 		return user;
 	}
-	
-	public boolean removeUser( User user ) throws IOException, StoreException {
+
+	public User detachOpenId(String openid, User user) throws IOException, SQLException {
 		CarPoolStore store = new CarPoolStoreImpl();
+		store.detachOpenID(openid, user.getUserId());
+		user.delOpenId(openid);
+
+		return user;
+	}
+
+	public boolean removeUser( User user ) throws IOException, StoreException, SQLException {
+		CarPoolStore store = new CarPoolStoreImpl();
+		boolean result = true;
 		for(String openid : user.getOpenIds()) {
-			System.out.println(openid);
-			store.detachOpenID(openid,user.getUserId());
+			result &= store.detachOpenID(openid,user.getUserId());
 		}
+		StringBuffer sql = new StringBuffer();
+		sql.append("delete from User where idUser = ");
+		sql.append(user.getUserId());
+		sql.append(";");
+		int count = 1;
+		Database db = new DatabaseImpl();
+		Statement statement = db.getStatement();
+		count = statement.executeUpdate(sql.toString());
+		statement.close();
 		
-		return store.removeUser(user.getUserName(), user.getPassword());
+		//store.removeUser(user.getUserName(), user.getPassword())
+		return count > 0 && result;
 	}
 }
