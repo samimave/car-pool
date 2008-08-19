2aa4acc31e67893faec40a25e9d45f4d89e99012
terrasea <terrasea@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-08-19 11:33:12 +0000

    Fixed some null pointer exceptions, I hope, in welcome.jsp.  Modified my code to make it a little more logical in design.

--- dev/web/WebContent/welcome.jsp
+++ dev/web/WebContent/welcome.jsp
@@ -124,7 +124,10 @@ if (request.getParameter("newUser") != null) {
 	<%@ include file="heading.html" %>
 
 	<DIV class="content">
-		<h2 align="center">Welcome to The Car Pool <%=user.getUserName()%><%//OpenIdFilter.getCurrentUser(request.getSession())%></h2>
+		<h2 align="center">Welcome to The Car Pool
+		<%if(session.getAttribute("signedin") != null ) {%>
+			<%=user.getUserName()%><%//OpenIdFilter.getCurrentUser(request.getSession())%></h2>
+		<%} %>
 		<%=message %>
 		<p>Eventually the person's upcoming rides will be displayed here.</p>
 	</DIV>		
--- dev/web/src/car/pool/user/UserManager.java
+++ dev/web/src/car/pool/user/UserManager.java
@@ -17,11 +17,52 @@ import java.util.Calendar;
 
 public class UserManager {
 
+	/**
+	 * Gets the users information from a database and fills the User class with this information and returns it. This method should only be used after the user has authenticated themselves with the openid provider.
+	 * @param openid - the openid the user uses to authenticate themselves
+	 * @return a User class instance encapsulating the information about the user
+	 * @throws InvaildUserNamePassword - if the openid does not exists in the database
+	 * @throws IOException - if connection errors happen
+	 * @throws SQLException - if the sql statement is malformed or there is something wrong with the database definition
+	 */
 	public User getUserByOpenId(String openid) throws InvaildUserNamePassword, IOException, SQLException {
-		return UserFactory.newInstance(openid);
+		User user = UserFactory.newInstance();
+		CarPoolStore store = new CarPoolStoreImpl();
+		user.setUserId(store.getUserIdByURL(openid));
+		Database db = new DatabaseImpl();
+		String sql = "select userName, email, mobile_number, signUpDate from User where idUser = " + user.getUserId() + ";";
+		Statement statement = db.getStatement();
+		ResultSet rs = statement.executeQuery(sql);
+		if(rs.first()) {
+			user.setUserName(rs.getString(1));
+			user.setEmail(rs.getString(2));
+			user.setPhoneNumber(rs.getString(3));
+			java.sql.Date date = rs.getDate(4);
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(date);
+			user.setMemberSince(cal);
+		}
+		for(String openids :store.getOpenIdsByUser(user.getUserId())) {
+			user.addOpenId(openids);
+		}
+
+		rs.close();
+		statement.close();
+		
+		return user;
 	}
 	
 
+	/**
+	 * Gets the users information from a database and fills the User class with this information and returns it.
+	 * It does this using the users username and password. It can be used to authenticate the user.
+	 * @param username - the users unique username
+	 * @param password - their password
+	 * @return a User class instance encapsulating the information about the user
+	 * @throws IOException - if connection errors happen
+	 * @throws InvaildUserNamePassword - if the username or password don't match what is in the database
+	 * @throws SQLException - if the sql statement is malformed or there is something wrong with the database definition
+	 */
 	public User getUserByUsername(String username, String password) throws IOException, InvaildUserNamePassword, SQLException {
 		User user = UserFactory.newInstance();
 		CarPoolStore store = new CarPoolStoreImpl();
@@ -46,6 +87,15 @@ public class UserManager {
 		return user;
 	}
 	
+	/**
+	 * Registers a user in the database based upon what is stored in the instance of the User class parameter
+	 * @param user - the User class instance containing the users information.
+	 * @return a User instance containing the users information plus the a new id for the user so now user.getUserId() will return something useful
+	 * @throws DuplicateUserNameException - if name already exist in the database
+	 * @throws UserException - User already exist or connection failed
+	 * @throws IOException - connection with the database errors
+	 * @throws SQLException - the sql statement encountered some problems
+	 */
 	public User registerUser(User user) throws DuplicateUserNameException, UserException, IOException, SQLException {
 		CarPoolStoreImpl store = new CarPoolStoreImpl();
 		Integer id = new Integer(store.addUserWithPassword(user.getUserName(), user.getEmail(), user.getPhoneNumber(), "n/a"));
@@ -61,6 +111,14 @@ public class UserManager {
 		return user;
 	}
 	
+	/**
+	 * Attach a openid to a already exiting user
+	 * @param openid - the openid to attach to the user
+	 * @param user - the user with all their details which to attach the openid to
+	 * @return the user with the openid now attached to them
+	 * @throws IOException - if connection errors occur
+	 * @throws SQLException - if something goes wrong with the sql statement
+	 */
 	public User attachOpenId(String openid, User user) throws IOException, SQLException {
 		CarPoolStore store = new CarPoolStoreImpl();
 		store.attachOpenID(openid, user.getUserId());
@@ -96,4 +154,12 @@ public class UserManager {
 		//store.removeUser(user.getUserName(), user.getPassword())
 		return count > 0 && result;
 	}
+	
+	
+/*	
+ 	public User updateUserDetails( User user ) {
+		String sql = "update User set email='%s', mobile_number='%s', ";
+		return user;
+	}
+*/
 }
