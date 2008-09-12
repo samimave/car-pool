299dfe2f6ca8e441c22ca90caaf22479fd494175
terrasea <terrasea@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-09-12 21:58:03 +0000

    Added in System.out.println statements, to OpenIdConsumer, to see if I can figure out what is going on from the stdout log on the server we are using at massey

--- trunk/dev/web/src/car/pool/user/authentication/servlet/OpenIdConsumer.java
+++ trunk/dev/web/src/car/pool/user/authentication/servlet/OpenIdConsumer.java
@@ -47,10 +47,13 @@ import car.pool.user.UserManager;
 	 */
 	//@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//phase one of authenticating a OpenId URL
+		
 		HttpSession session = request.getSession();
 		
+		System.out.println("OpenIdConsumer: start");
+		//phase one of authenticating a OpenId URL
 		if(OpenIdFilter.getCurrentUser(session) == null && request.getParameter("openid_url") != null) {
+			System.out.println("OpenIdConsumer: phase 1");
 			try {
 				//The url that is going to be returned to after the OpenId has been verified or not
 				StringBuffer returnTo = new StringBuffer(UrlUtils.getBaseUrl(request));
@@ -69,10 +72,12 @@ import car.pool.user.UserManager;
 				//The web site url that you want the OpenId provider to trust
 				String trustRoot = UrlUtils.getBaseUrl(request);
 				//The url plus query string is created here for redirection purposes
+				System.out.println(String.format("OpenIdConsumer: id: %s, returnTo: %s, trustRoot %s", id, returnTo.toString(), trustRoot));
 				String s = OpenIdFilter.joid().getAuthUrl(id, returnTo.toString(), trustRoot);
 				response.sendRedirect(s);
 				return;
 			} catch (OpenIdException e) {
+				System.out.println(String.format("OpenIdConsumer: OpenIdException %s", e.toString()));
 				// Something bad happened, don't know what it is but will redirect to index
 				//HttpSession session = request.getSession();
 				session.setAttribute("OpenIdException", e.toString());
@@ -88,6 +93,7 @@ import car.pool.user.UserManager;
 			  * redirects back to this page after confirming or denying a authentication
 			  * request
 			  */
+			System.out.println(String.format("OpenIdConsumer: phase 2"));
 			/*
 			 * try to get a OpenId URL, if the request was refused then a session
 			 * attribute will not be set so will return null otherwise it will return
@@ -101,6 +107,7 @@ import car.pool.user.UserManager;
 			if(loggedInAs != null) {
 				// The OpenId provider authenticated this user
 				// create a instance of UserManager
+				System.out.println(String.format("OpenIdConsumer: loggedInAs %s", loggedInAs));
 				UserManager manager = new UserManager();
 				try {
 					// get users info from the database and put it into the session attributes along with signedin
@@ -117,6 +124,7 @@ import car.pool.user.UserManager;
 				} catch (InvaildUserNamePassword e) {
 					// OpenId not in database so redirect to register
 					//HttpSession session = request.getSession();
+					System.out.println(String.format("OpenIdConsumer: InvalidUserNamePassword: %s", e.toString()));
 					session.setAttribute("InvalidUserNamePassword", e.toString());
 					StringBuffer buff = new StringBuffer();
 					buff.append(request.getContextPath());
@@ -125,10 +133,12 @@ import car.pool.user.UserManager;
 					return;
 				} catch (SQLException e) {
 					// TODO redirect to error page
+					System.out.println(String.format("OpenIdConsumer: SQLException: %s", e.toString()));
 				}
 			} else {
 				// Log in failed go back to index
 				//HttpSession session = request.getSession();
+				System.out.println(String.format("OpenIdConsumer: Not Authenticated"));
 				session.setAttribute("Not Authenticated", "Not gonna happen");
 				StringBuffer buff = new StringBuffer();
 				buff.append(request.getContextPath());
