30bfa04f0fe65869210e2a6f474b1bdaa03931d9
terrasea <terrasea@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-10-15 21:08:56 +0000

    SMTPException.java commented

--- trunk/dev/web/src/car/pool/email/SMTPException.java
+++ trunk/dev/web/src/car/pool/email/SMTPException.java
@@ -3,6 +3,14 @@ package car.pool.email;
 public class SMTPException extends Exception {
 	private static final long serialVersionUID = 2355637793982227933L;
 
+	/**
+	 * A unified Exception class used through the car.pool.email
+	 * package as a unified Excetion to throw, so code only has to
+	 * worry about this one and if the library used to send email's
+	 * changes they still don't have to change the code as this will
+	 * still be used
+	 * @param reason - The reason the exception is thrown is stated here.
+	 */
 	public SMTPException(String reason) {
 		super(reason);
 	}
