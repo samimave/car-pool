15ccbb05f4b19d25bd533337229ab188d82a30fa
terrasea <terrasea@6f200791-1c52-0410-aeab-7b5676d02d99>
2008-10-15 13:16:23 +0000

    Email.java is now commented

--- trunk/dev/web/src/car/pool/email/Email.java
+++ trunk/dev/web/src/car/pool/email/Email.java
@@ -8,11 +8,15 @@ import car.pool.persistance.Database;
 import car.pool.persistance.DatabaseImpl;
 
 public class Email {
+	//the email variables
 	private String to = null;
 	private String from = null;
 	private String subject = null;
 	private String message = null;
 	
+	/**
+	 * Connects to database and fetches the from address from the Email table.  		 * This then becomes the from address used when sending emails
+	 */
 	public Email() {
 		try {
 			Database db = new DatabaseImpl();
@@ -33,39 +37,67 @@ public class Email {
 		
 	}
 	
+	/**
+	 * Sets the recipients address
+	 * @param to - email address of the recipient
+	 */
 	public void setToAddress(String to) {
 		this.to = to;
 	}
 	
-	
+	/**
+	 * Gets the email address of the recipient of the email
+	 * @return - the recipients email address
+	 */
 	public String getToAddress() {
 		return to;
 	}
 	
+	/**
+	 * sets the senders address
+	 * @param from - the senders email address
+	 */
 	private void setFromAddress(String from) {
 		this.from = from;
 	}
 	
-	
+	/**
+	 * Gets the senders email address
+	 * @return - the senders email address
+	 */
 	public String getFromAddress() {
 		return from;
 	}
 	
 	
+	/**
+	 * Sets the subject line of the email
+	 * @param subject - the subject of the email
+	 */
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
 	
-	
+	/**
+	 * Gets the subject of the email
+	 * @return - the subject of the email
+	 */
 	public String getSubject() {
 		return subject;
 	}
 	
+	/**
+	 * Sets the message or the main body of the email
+	 * @param message - the message the sender wants to communicate with the receiver
+	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
 	
-	
+	/**
+	 * Gets the message to be sent
+	 * @return - the message to be sent
+	 */
 	public String getMessage() {
 		return message;
 	}
